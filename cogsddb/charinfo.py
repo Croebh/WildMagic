32e31b3c42c399b3555291685d7a068a0ced7b14
Joseph Keen <icrona@gmail.com>
2022-04-30 20:34:13 -0600

    Rearrange some things, add desc issue detection, add language and feats

--- cogsddb/charinfo.py
+++ cogsddb/charinfo.py
@@ -1,10 +1,8 @@
 import json
 import re
-from typing import Optional
+from typing import Optional, Tuple, List
 
 import requests
-
-from utils import utils
 
 from disnake.ext import commands
 
@@ -14,39 +12,61 @@ class CharInfo(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    async def new_char(self, ctx: commands.Context, url: str):
+        """Generates a multiline command that assist with creating new characters,
+        given a provided DDB character sheet."""
+        data = await self.get_character_data(ctx, url)
+        desc, issues = await self._get_desc(ctx, url, data)
+        out = [i for i in [desc,
+                           await self._get_bags(ctx, url, data),
+                           await self._get_tools(ctx, url, data),
+                           await self._get_languages(ctx, url, data),
+                           await self._get_feats(ctx, url, data)]
+               if i]
+        if issues:
+            await ctx.send(f"**Issues found:**\n{', '.join(issues)}")
+        await ctx.send("Copy and paste the following command into this channel:\n```py\n!multiline\n"
+                       + '\n'.join(out)
+                       + "\n```")
+
+    @commands.command()
     async def get_desc(self, ctx: commands.Context, url: str):
         """Generates a command to set up your characters description, given a provided DDB character sheet."""
-        # await utils.try_delete(ctx.message)
-        out = await self._get_desc(ctx, url)
-        await ctx.send(f"```py\n{out}\n```")
+        out, issues = await self._get_desc(ctx, url)
+        await ctx.send(f"Copy and paste the following command into this channel:\n```py\n{out}\n```")
+        if issues:
+            await ctx.send(f"**Issues found:**\n{', '.join(issues)}")
+
+    @commands.command()
+    async def get_languages(self, ctx: commands.Context, url: str):
+        """Generates a command to set up your characters languages, given a provided DDB character sheet."""
+        out = await self._get_languages(ctx, url)
+        await ctx.send(f"Copy and paste the following command into this channel:\n```py\n{out}\n```")
+
+    @commands.command()
+    async def get_feats(self, ctx: commands.Context, url: str):
+        """Generates a command to set up your characters feats, given a provided DDB character sheet."""
+        out = await self._get_feats(ctx, url)
+        if not out:
+            await ctx.send("No feats found.")
+            return
+        await ctx.send(f"Copy and paste the following command into this channel:\n```py\n{out}\n```")
 
     @commands.command()
     async def get_tools(self, ctx: commands.Context, url: str):
         """Generates a command to set up your tool proficiencies for `!tool`, given a provided DDB character sheet."""
-        # await utils.try_delete(ctx.message)
+
         out = await self._get_tools(ctx, url)
         if not out:
             await ctx.send("No tool proficiencies found.")
             return
-        await ctx.send(f"```py\n{out}\n```")
+        await ctx.send(f"Copy and paste the following command into this channel:\n```py\n{out}\n```")
 
     @commands.command()
     async def get_bags(self, ctx: commands.Context, url: str):
         """Generates a command to set up the `!bag` alias, given a provided DDB character sheet."""
-        # await utils.try_delete(ctx.message)
         out = await self._get_bags(ctx, url)
-        await ctx.send(f"```py\n{out}\n```")
-
-    @commands.command()
-    async def new_char(self, ctx: commands.Context, url: str):
-        """Generates a multiline command that assist with creating new characters,
-        given a provided DDB character sheet."""
-        # await utils.try_delete(ctx.message)
-        out = [i for i in [await self._get_desc(ctx, url),
-                           await self._get_bags(ctx, url),
-                           await self._get_tools(ctx, url)]
-               if i]
-        await ctx.send("```py\n!multiline\n" + '\n'.join(out) + "\n```")
+        await ctx.send(f"Copy and paste the following command into this channel:\n```py\n{out}\n```")
 
     @staticmethod
     async def get_character_data(ctx: commands.Context, url: str):
@@ -62,13 +82,13 @@ class CharInfo(commands.Cog):
         json_data = json.loads(resp.content)['data']
         return json_data
 
-    async def _get_desc(self, ctx: commands.Context, url: str) -> str:
+    async def _get_desc(self, ctx: commands.Context, url: str, data: dict = None) -> tuple[str, list[str]]:
 
         alignments = ["LG", "NG", "CG",
                       "LN", "TN", "CN",
                       "LE", "NE", "CE"]
-
-        data = await self.get_character_data(ctx, url)
+        if data is None:
+            data = await self.get_character_data(ctx, url)
 
         classes = []
         for _class in data['classes']:
@@ -78,7 +98,7 @@ class CharInfo(commands.Cog):
             classes.append(cur_class)
         out = {
             "height": data["height"] or "height",
-            "weight": f'{data["weight"]} lb.' or "weight",
+            "weight": f'{data["weight"]} lb.' if data["weight"] else "weight",
             "race": data['race']['fullName'] or "race",
             "class": '/'.join(classes) or "class",
             "appearance": data['traits'][
@@ -92,23 +112,33 @@ class CharInfo(commands.Cog):
             "alignment": alignments[data['alignmentId'] - 1] if data[
                 'alignmentId'] else "Enter your alignment"
         }
+        issues = []
+        if out["height"] == "height":
+            issues.append("Height not set")
+        if out["weight"] == "weight":
+            issues.append("Weight not set")
+        if out["appearance"] == "Write a bit about attitude, appearance, and background here.":
+            issues.append("Description not set")
+        for trait in ("traits", "ideals", "bonds", "flaws"):
+            if "Enter your D&D Beyond rolled trait(s) here" in out[trait]:
+                issues.append(f"{trait.title()} not set")
         desc_out = f"""!desc update __**{out['height']} | {out['weight']} | {out['race']} | {out['class']}**__
-                       > {out["appearance"]}
+                       > {out["appearance"].strip()}
                        **Personality Traits**
-                       {out["traits"]}
+                       {out["traits"].strip()}
                        **Ideals**
-                       {out["ideals"]}
+                       {out["ideals"].strip()}
                        **Bonds**
-                       {out["bonds"]}
+                       {out["bonds"].strip()}
                        **Flaws**
-                       {out["flaws"]}
+                       {out["flaws"].strip()}
                        **Alignment**
-                       {out["alignment"]}"""
-        return '\n'.join([line.strip() for line in desc_out.splitlines()])
+                       {out["alignment"].strip()}"""
+        return '\n'.join([line.strip() for line in desc_out.splitlines()]), issues
 
-    async def _get_tools(self, ctx: commands.Context, url: str) -> Optional[str]:
-
-        data = await self.get_character_data(ctx, url)
+    async def _get_tools(self, ctx: commands.Context, url: str, data: dict = None) -> Optional[str]:
+        if data is None:
+            data = await self.get_character_data(ctx, url)
 
         profs = []
         expertise = []
@@ -127,9 +157,34 @@ class CharInfo(commands.Cog):
             out.append(f"""!cvar eTools {', '.join(expertise)}""")
         return '\n'.join(out) or None
 
-    async def _get_bags(self, ctx: commands.Context, url: str) -> str:
+    async def _get_languages(self, ctx: commands.Context, url: str, data: dict = None) -> Optional[str]:
+        if data is None:
+            data = await self.get_character_data(ctx, url)
 
-        data = await self.get_character_data(ctx, url)
+        languages = []
+        for _type in data['modifiers']:
+            for modifier in data['modifiers'][_type]:
+                # We only care about languages
+                if modifier['entityTypeId'] == 906033267:
+                    languages.append(modifier['friendlySubtypeName'])
+
+        return f"""!cvar languages {', '.join(languages)}"""
+
+    async def _get_feats(self, ctx: commands.Context, url: str, data: dict = None) -> Optional[str]:
+        if data is None:
+            data = await self.get_character_data(ctx, url)
+
+        feats = []
+        for _feat in data['feats']:
+            feats.append(_feat['definition']['name'])
+
+        if feats:
+            return f"""!cvar feats {', '.join(feats)}"""
+
+    async def _get_bags(self, ctx: commands.Context, url: str, data: dict = None) -> str:
+
+        if data is None:
+            data = await self.get_character_data(ctx, url)
 
         out = {
             "Backpack": {},
